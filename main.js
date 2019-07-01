bbff16699a703783824a21262dfe7e2599842ec1
Victor Palma <victor@gofixchile.com>
2019-07-01 09:50:19 -0400

    Fix root folder

--- main.js
+++ main.js
@@ -1,30 +1,55 @@
 
-const {app, BrowserWindow, dialog} = require('electron');
+const {app, BrowserWindow, dialog, Menu} = require('electron');
 const {glob} = require('glob');
 const {readFileSync} = require('fs');
 
 let mainWindow;
 function createWindow (launchInfo) {
   mainWindow = new BrowserWindow({
-    title: 'MAT PG Admin',
-    icon: 'img/favicon.ico',
+    title: 'mat-pg-admin-4',
+    icon: 'img/elephant.png',
     width: 800,
     height: 600,
+    "skip-taskbar": false,
+    "auto-hide-menu-bar": false,
+    "enable-larger-than-screen": false,
+    frame: false,
     webPreferences: {
       nodeIntegration: false,
-      devTools : true 
+      devTools : true
     }
   });
-
-  //mainWindow.webContents.openDevTools({mode: 'detach'})  
-  let addr_path = "/Users/Victor\ Palma/.pgAdmin4*.addr";
-  if( process.platform === 'linux' ){
-    addr_path = "/home/donvictor/.pgAdmin4*.addr";
+  if (process.platform === 'darwin') {
+    // Create our menu entries so that we can use MAC shortcuts
+    Menu.setApplicationMenu(Menu.buildFromTemplate([
+      {
+        label: 'App',
+        submenu: [
+          { role: 'undo' },
+          { role: 'redo' },
+          { type: 'separator' },
+          { role: 'cut' },
+          { role: 'copy' },
+          { role: 'paste' },
+          { role: 'pasteandmatchstyle' },
+          { role: 'delete' },
+          { role: 'selectall' },
+          { role: 'close' }
+        ]
+      }
+    ]));
   }
+
+  mainWindow.webContents.on('did-finish-load', function() {
+    mainWindow.webContents.insertCSS('.pg-navbar .navbar-brand .app-icon{ -webkit-user-select: none; -moz-user-select: none; -ms-user-select: none; user-select: none; -webkit-app-region: drag;');
+  });
+
+
+  //mainWindow.webContents.openDevTools({mode: 'detach'});
+  let addr_path = app.getPath('home')+"/.pgAdmin4*.addr";
+
   glob(addr_path, {}, (err, files) => {
 
-    //mainWindow.webContents.executeJavaScript("console.log('err', "+JSON.stringify(err)+")");
-    //mainWindow.webContents.executeJavaScript("console.log('files', "+JSON.stringify(files)+")");
     if( err || files.length == 0 ){
       dialog.showMessageBox(mainWindow, {
         message: 'Primero debes abrir PG Admin. ('+process.platform+')',
@@ -45,7 +70,7 @@ function createWindow (launchInfo) {
         type: 'question',
         buttons: ['Salir', 'Cancelar'],
         title: 'Estás seguro que deseas salir?',
-        message: 'Perderas los ultimos cambios que no has guardado.',
+        message: 'Perderás los ultimos cambios que no has guardado.',
         defaultId: 0,
         cancelId: 1
       })
